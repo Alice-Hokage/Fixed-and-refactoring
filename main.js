53ac19e2e9b86f18af888d1850a761f59ca7cd6f
Alice-Hokage <AliceFedoseeva@yandex.ru>
2020-06-23 13:40:19 +0300

    delete temporary variable

--- main.js
+++ main.js
@@ -1,38 +1,50 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = 'Счет для ${invoice.customer}\n';
-  const format = new Inti.NumberFormat("ru-RU",
-  { style: "currency", currency: "RUB",
-  minimumFractionDigits: 2 }).format;
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = 'Счет для ${invoice.customer}\n';
+const format = new Inti.NumberFormat("ru-RU",
+        { style: "currency", currency: "RUB",
+        minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-  const play = plays[perf.playlD];
-  let thisAmount = 0;
-  switch (play.type) {
-  case "tragedy":
-  thisAmount = 40000;
-  if (perf.audience > 30) {
-  thisAmount += 1000 * (perf.audience - 30);
-  }
-  break;
-  case "comedy":
-  thisAmount = 30000;
-  if (perf.audience > 20) {
-  thisAmount += 10000 + 500 * (perf.audience - 20);
-  }
-  thisAmount += 300 * perf.audience;
-  break;
-  default:
-  throw new Error('неизвестный тип: ${play.type}');
-  }
+      let thisAmount = amountFor(perf);
+
   // Добавление бонусов
-  volumeCredits += math.max(perf.audience - 30, 0);
+      volumeCredits += math.max(perf.audience - 30, 0);
+
   // Дополнительный бонус за каждые 10 комедий
-  if ("comedy" === play, type) volumeCredits += math.floor(perf.audience / 5);
+      if ("comedy" === playFor(perf).type)
+          volumeCredits += math.floor(perf.audience / 5);
   // Вывод строки счета
-  result += ' ${play.name}: ${format(thisAmount / 100)}';
-  result += ' (${perf.audience} мест)\n';
-  totalAmount += thisAmount;
+      result += ' ${playFor(perf).name}: ${format(thisAmount / 100)}';
+      result += ' (${perf.audience} мест)\n';
+      totalAmount += thisAmount;
+  }
   result += 'Итого с вас $(format(totalAmount/100)}\n';
   result += 'Вы заработали ${volumeCredits} бонусов\n';
-  return result;
+    return result;
+  function amountFor(performance) {
+    let quantity = 0;
+  switch (playFor(performance).type) {
+    case "tragedy":
+      quantity = 40000;
+      if (performance.audience > 30) {
+      quantity += 1000 * (performance.audience - 30);
+    }
+
+      break;
+
+    case "comedy":
+      quantity = 30000;
+
+      if (performance.audience > 20) {
+      quantity += 10000 + 500 * (performance.audience - 20);
+    }
+    quantity += 300 * performance.audience;
+    break;
+    
+    default:
+      throw new Error('неизвестный тип: ${playFor(performance).type}');
+  }
+  return quantity;
+}
+}
