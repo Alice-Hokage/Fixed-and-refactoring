88947bc1055f889597dcac851ede54207647e533
Alice-Hokage <AliceFedoseeva@yandex.ru>
2020-06-23 14:18:52 +0300

    bonus reform

--- main.js
+++ main.js
@@ -1,50 +1,54 @@
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
-    let result = 'Счет для ${invoice.customer}\n';
+    let result = `Счет для ${invoice.customer}\n`;
 const format = new Inti.NumberFormat("ru-RU",
         { style: "currency", currency: "RUB",
         minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-      let thisAmount = amountFor(perf);
 
-  // Добавление бонусов
-      volumeCredits += math.max(perf.audience - 30, 0);
-
-  // Дополнительный бонус за каждые 10 комедий
-      if ("comedy" === playFor(perf).type)
-          volumeCredits += math.floor(perf.audience / 5);
+      volumeCredits += volumeCreditsFor(perf);
   // Вывод строки счета
-      result += ' ${playFor(perf).name}: ${format(thisAmount / 100)}';
-      result += ' (${perf.audience} мест)\n';
-      totalAmount += thisAmount;
+      result += ` ${playFor(perf).name}: `;
+      result += `${format(amountFor(perf) / 100)} `;
+      result += ` (${perf.audience} мест)\n`;
+      totalAmount += amountFor(perf);
   }
-  result += 'Итого с вас $(format(totalAmount/100)}\n';
-  result += 'Вы заработали ${volumeCredits} бонусов\n';
+  result += `Итого с вас ${format(totalAmount/100)}\n`;
+  result += `Вы заработали ${volumeCredits} бонусов\n`;
     return result;
   function amountFor(performance) {
-    let quantity = 0;
-  switch (playFor(performance).type) {
-    case "tragedy":
-      quantity = 40000;
-      if (performance.audience > 30) {
-      quantity += 1000 * (performance.audience - 30);
-    }
+      let quantity = 0;
+    switch (playFor(performance).type) {
+      case "tragedy":
+        quantity = 40000;
+        if (performance.audience > 30) {
+        quantity += 1000 * (performance.audience - 30);
+      }
 
+        break;
+
+      case "comedy":
+        quantity = 30000;
+
+        if (performance.audience > 20) {
+        quantity += 10000 + 500 * (performance.audience - 20);
+      }
+      quantity += 300 * performance.audience;
       break;
-
-    case "comedy":
-      quantity = 30000;
-
-      if (performance.audience > 20) {
-      quantity += 10000 + 500 * (performance.audience - 20);
+      
+      default:
+        throw new Error('неизвестный тип: ${playFor(performance).type}');
     }
-    quantity += 300 * performance.audience;
-    break;
-    
-    default:
-      throw new Error('неизвестный тип: ${playFor(performance).type}');
+    return quantity;
   }
-  return quantity;
-}
+  function volumeCreditsFor(performance) {
+    let quantity = 0;
+    quantity += Math.max(performance.audience - 30, 0);
+
+    if ("comedy" === playFor(performance).type)
+      quantity += Math.floor(performance.audience / 5);
+
+    return quantity;
+  }
 }
